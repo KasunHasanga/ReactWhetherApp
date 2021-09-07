e9eb71e5defe456cc8a80450ea32c9ae7f273097
KasunHasanga <kasunhasanga96@gmail.com>
2021-09-07 15:37:22 +0530

    Get whether data from backend and store in locally

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,12 +1,12 @@
 import React, { useState, useEffect } from "react";
 import WhetherCard from "../WhetherCard/WhetherCard";
 import axios from "axios";
-import { CircularProgress } from "@material-ui/core";
+import { CircularProgress, Grid } from "@material-ui/core";
 
 const Home = () => {
   const url = "http://api.openweathermap.org/data/2.5/group";
   const api_key = "374ae00a2b8c9fd0840c0dcd7bc81eb7";
-  const [whether, setWhether] = useState(null);
+  const [whetherData, setwhetherData] = useState(null);
   const params = {
     id: "1248991,1850147,2644210,2988507,2147714,4930956,1796236,3143244",
     appid: api_key,
@@ -21,7 +21,7 @@ const Home = () => {
       config: { headers: { "Content-Type": "multipart/form-data" } },
     })
       .then(function (response) {
-        setWhether(response.data);
+        setwhetherData((response.data.list));
         //handle success
       })
       .catch(function (response) {
@@ -30,14 +30,28 @@ const Home = () => {
       });
   }, [url]);
 
-  console.log(whether);
   console.log("-----------");
-  return !whether ? (
+  console.log(whetherData);
+  console.log("-----------");
+  return !whetherData ? (
     <CircularProgress />
   ) : (
     <div>
-      <WhetherCard />
-
+        <Grid
+    //   className={classes.container}
+      container
+      alignItems="stretch"
+      spacing={3}
+    >
+{whetherData.map((whetherSingleData) => (
+        // <Grid key={whetherSingleData._id} item xs={12} sm={6} md={6} lg={3}>
+         
+        // </Grid>
+        <WhetherCard whetherData={whetherSingleData}/>
+      ))}
+        </Grid>
+      
+      
       
     </div>
   );
--- src/components/WhetherCard/WhetherCard.js
+++ src/components/WhetherCard/WhetherCard.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect }from 'react';
+import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
@@ -33,20 +33,9 @@ const useStyles = makeStyles({
   },
 });
 
-export default function WhetherCard() {
+export default function WhetherCard({whetherData}) {
   const classes = useStyles();
-
-  const [data, setDate] = useState(null)
-
-//   useEffect(() => {
-//       //
-
-//       setDate(data.response);
-//   }, [])
-
-//   if(!data){
-//       return <>Loading</>
-//   }
+  console.log(whetherData);
 
   return (
     <Card className={classes.root}>
